a961e2d20b1ff06f37a130175c90736817aa656e
ang67 <binikouadiojosias@gmail.com>
2020-04-18 22:56:58 +0200

    intégration simple de google map

--- lib/ui/screens/statsPages/mapPage.dart
+++ lib/ui/screens/statsPages/mapPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapTabPage extends StatefulWidget {
   @override
@@ -6,10 +8,28 @@ class MapTabPage extends StatefulWidget {
 }
 
 class _MapTabPageState extends State<MapTabPage> {
+  Completer<GoogleMapController> _controller = Completer();
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(47.255573, 2.667633),
+    zoom: 3.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(47.255573, 2.667633),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Icon(Icons.map, size: 64.0, color: Colors.blue)),
+    return GoogleMap(
+      
+      scrollGesturesEnabled: true,
+      minMaxZoomPreference: MinMaxZoomPreference(1, 20),
+      mapType: MapType.normal,
+      initialCameraPosition: _kGooglePlex,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
     );
   }
 }
