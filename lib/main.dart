4bf620dc64e7bd14ced46e6f705f1b8c7e9c417d
ang67 <binikouadiojosias@gmail.com>
2020-04-24 01:15:41 +0200

    fixer l'écran en mode portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:covidapp/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+    .then((_) {
+      runApp(new MyApp());
+    });
+}
